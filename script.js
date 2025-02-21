782fdd19cd3d570abfde6d5fa766b4eeabbd9a0c
Harshal Bhute <harshalbhute39@gmail.com>
2025-02-21 11:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const byteSize = (str) => {
   // write your code here
-	let Blob = {
-		data:str,
-		size:data.size();
-	}
-	return Blob.size;
+	return new Blob([str]).size
 };
 
 // Do not change the code below
